cd3d66dd2a85886e7ecae265b87361a162fcbc8a
belmonmi <Michael.Belmont@Outlook.com>
2022-08-26 10:33:29 -0500

    Grid control is not showing in Shell.FlyoutFooterTemplate

---
+++ AppShellFooter/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppShellFooter;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShellFooter/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppShellFooter;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
